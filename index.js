729580d964001525579948c63eb580b79ee9b183
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-11-26 13:54:59 +0900

    날짜 요일 시간

---
+++ index.js
@@ -0,0 +1,24 @@
+// 시계
+const clock = document.body.querySelector(".clock");
+const toDay = document.body.querySelector(".today");
+
+//
+
+// 요일 구하기 위한 주
+
+const week = ["월", "화", "수", "목", "금", "토", "일"];
+
+const clockFunc = () => {
+  let date = new Date();
+  let mon = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = String(date.getHours()).padStart(2, "0");
+  let min = String(date.getMinutes()).padStart(2, "0");
+  let sec = String(date.getSeconds()).padStart(2, "0");
+  let yoil = date.getDay() - 1;
+
+  toDay.innerHTML = `${mon}월 ${day}일 ${week[yoil]}요일`;
+  clock.innerHTML = `${hour} : ${min} : ${sec}`;
+};
+
+setInterval(clockFunc, 1000);
