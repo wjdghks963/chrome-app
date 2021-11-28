e310981fa338c39f1471843ef85ecc2b6902a3c9
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-11-28 18:26:09 +0900

    배운거 주석처리

--- funcs/todo.js
+++ funcs/todo.js
@@ -12,12 +12,14 @@ const saveTodos = () => {
 const deleteTodos = (e) => {
   const li = e.target.parentElement;
   li.remove();
+  // todoArr엔 id가 맞지 않은 남은 것들로 다시 정의
   todoArr = todoArr.filter((todos) => todos.id !== parseInt(li.id));
   saveTodos();
 };
 
 const putTodos = (todo) => {
   const li = document.createElement("li");
+  li.id = todo.id;
   const span = document.createElement("span");
   li.appendChild(span);
   span.innerText = todo.text;
@@ -53,6 +55,7 @@ const getTodos = localStorage.getItem("todos");
 
 if (getTodos !== null) {
   const parsedTodos = JSON.parse(getTodos);
+  // todoArr에 parsedTodos를 넣어서 전에 있던 todo들을 복원함(localstorage로 부터)
   todoArr = parsedTodos;
   parsedTodos.forEach(putTodos);
 }
