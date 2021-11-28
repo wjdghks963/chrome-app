3cd6bd69810c6e0c9d56636c5b61b542c062c045
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-11-28 18:09:51 +0900

    todos localstorage에 추가와 삭제 기능

--- funcs/clock.js
+++ funcs/clock.js
@@ -4,7 +4,7 @@ const toDay = document.body.querySelector(".today");
 
 // 요일 구하기 위한 주
 
-const week = ["월", "화", "수", "목", "금", "토", "일"];
+const week = ["일", "월", "화", "수", "목", "금", "토"];
 
 const clockFunc = () => {
   let date = new Date();
@@ -13,7 +13,7 @@ const clockFunc = () => {
   let hour = String(date.getHours()).padStart(2, "0");
   let min = String(date.getMinutes()).padStart(2, "0");
   let sec = String(date.getSeconds()).padStart(2, "0");
-  let yoil = date.getDay() - 1;
+  let yoil = date.getDay();
 
   toDay.innerHTML = `${mon}월 ${day}일 ${week[yoil]}요일`;
   clock.innerHTML = `${hour} : ${min} : ${sec}`;
--- funcs/todo.js
+++ funcs/todo.js
@@ -3,20 +3,56 @@ const todoInput = todoBox.querySelector("input");
 const saveBtn = todoBox.querySelector(".saveBtn");
 const todoList = todoBox.querySelector(".todoList");
 
+let todoArr = [];
+
+const saveTodos = () => {
+  localStorage.setItem("todos", JSON.stringify(todoArr));
+};
+
+const deleteTodos = (e) => {
+  const li = e.target.parentElement;
+  li.remove();
+  todoArr = todoArr.filter((todos) => todos.id !== parseInt(li.id));
+  saveTodos();
+};
+
 const putTodos = (todo) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
   li.appendChild(span);
+  span.innerText = todo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodos);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
-  span.innerText = todo;
 };
 
 const submitTodo = (e) => {
   e.preventDefault();
   const todo = todoInput.value;
   todoInput.value = "";
+  const todoObj = {
+    text: todo,
+    id: Date.now(),
+  };
 
-  putTodos(todo);
+  if (todo === "") {
+    alert("아무것도 없어요!");
+  } else {
+    todoArr.push(todoObj);
+    putTodos(todoObj);
+    saveTodos();
+  }
 };
 
 saveBtn.addEventListener("click", submitTodo);
+
+const getTodos = localStorage.getItem("todos");
+
+if (getTodos !== null) {
+  const parsedTodos = JSON.parse(getTodos);
+  todoArr = parsedTodos;
+  parsedTodos.forEach(putTodos);
+}
